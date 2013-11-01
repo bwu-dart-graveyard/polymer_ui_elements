5582905b0c48ac954ec0ff4b74ecf64b90421365
zoechi <gzoechi@gmail.com>
2013-11-01 12:12:21 +0100

    closes #8 fix build.dart

--- build.dart
+++ build.dart
@@ -1,9 +1,10 @@
 import 'package:polymer/builder.dart';
 
-void main() {
+void main(args) {
   build(entryPoints: [
                       'example/polymer_ui_arrow_demo.html',
                       'example/polymer_ui_icon_button.html',
                       'example/ui_arrow_example.html'
-                      ]);
+                      ],
+                      options: parseOptions(args));
 }
