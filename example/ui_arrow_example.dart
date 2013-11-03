2912bfc8f1cdb8e5849bc07c9d095151c48821d8
zoechi <gzoechi@gmail.com>
2013-11-03 12:37:21 +0100

    #4 fix license headers

--- example/ui_arrow_example.dart
+++ example/ui_arrow_example.dart
@@ -1,7 +1,8 @@
 // Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
 // the AUTHORS file for details. All rights reserved. Use of this source code is 
 // governed by a BSD-style license that can be found in the LICENSE file.
-
+// This work is a port of the polymer-elements from the Polymer project, 
+// http://www.polymer-project.org/. 
 library polymer.elements.ui_arrow_example;
 
 import 'package:polymer/polymer.dart';
