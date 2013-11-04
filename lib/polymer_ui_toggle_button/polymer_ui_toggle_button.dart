c7fa8a48898c18c0c731bf147f4f1b12b9bcc6d8
zoechi <gzoechi@gmail.com>
2013-11-04 12:20:08 +0100

    #41 button should show a caption after page load

--- lib/polymer_ui_toggle_button/polymer_ui_toggle_button.dart
+++ lib/polymer_ui_toggle_button/polymer_ui_toggle_button.dart
@@ -56,6 +56,11 @@ class PolymerUiToggleButton extends PolymerElement {
   int _w;
   int _x;
   
+  @override 
+  void enteredView() {
+    updateCaption();
+    super.enteredView();
+  }
   void toggle() {
     this.value = !this.value;
   }
