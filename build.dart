f7feec41728a9bc3652f5657db0fcf7c54495bbc
zoechi <gzoechi@gmail.com>
2013-11-04 12:18:50 +0100

    created symbolic link from example to web as a workaround to make pub build work

--- build.dart
+++ build.dart
@@ -2,9 +2,17 @@ import 'package:polymer/builder.dart';
 
 void main(args) {
   build(entryPoints: [
-                      'example/polymer_ui_arrow_demo.html',
+                      'web/polymer_ui_arrow_demo.html',
+                      'web/polymer_ui_icon_button.html',
+                      'web/polymer_ui_toggle_button.html',
+                      'web/polymer_ui_toolbar.html',
+                      'web/ui_arrow_example.html'
+/*                      'example/polymer_ui_arrow_demo.html',
                       'example/polymer_ui_icon_button.html',
+                      'example/polymer_ui_toggle_button.html',
+                      'example/polymer_ui_toolbar.html',
                       'example/ui_arrow_example.html'
+*/
                       ],
                       options: parseOptions(args));
 }
