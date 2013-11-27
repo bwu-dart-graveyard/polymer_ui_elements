256e36e5f44fd93d71238feb19576be92ca20c13
Erik Grimes <erik.grimes@gmail.com>
2013-11-26 21:29:49 -0600

    Fix #60

---
+++ example/src/polymer_ui_icon/app_element.dart
@@ -0,0 +1,24 @@
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
+// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// governed by a BSD-style license that can be found in the LICENSE file.
+// This work is a port of the polymer-elements from the Polymer project, 
+// http://www.polymer-project.org/. 
+
+library app_element;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('app-element')
+class AppElement extends PolymerElement {
+  
+  @observable
+  List icons;
+  
+  AppElement.created() : super.created();
+  
+  enteredView(){
+    icons = document.createElement('polymer-ui-icon').icons.keys.toList();
+  }
+  
+}
