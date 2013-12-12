900898bd4c5207e7ce160d77d2ee89d1971bb824
zoechi <gzoechi@gmail.com>
2013-12-12 14:11:20 +0100

    fixes #40 fixes #87

---
+++ lib/polymer_ui_tabs/polymer_ui_tabs.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
+// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// governed by a BSD-style license that can be found in the LICENSE file.
+// This work is a port of the polymer-elements from the Polymer project, 
+// http://www.polymer-project.org/. 
+library polymer_ui_elements.polymer_ui_tabs;
+
+import 'package:polymer/polymer.dart' show CustomTag, PolymerElement, published,
+ChangeNotifier, reflectable; // TODO remove ChangeNotifier, reflectable when bug is solved  
+// https://code.google.com/p/dart/issues/detail?id=13849
+// (https://code.google.com/p/dart/issues/detail?id=15095)
+import 'package:logging/logging.dart' show Logger;
+import 'package:polymer_elements/polymer_selector/polymer_selector.dart' show PolymerSelector;
+
+@CustomTag('polymer-ui-tabs')
+class PolymerUiTabs extends PolymerSelector {
+  PolymerUiTabs.created() : super.created() {
+    _logger.finest('created');
+  }
+
+  final _logger = new Logger('polymer-ui-tabs');
+}
