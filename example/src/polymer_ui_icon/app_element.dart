672ddadd9d189ed15c93b7000e33b6ed71a9b122
zoechi <gzoechi@gmail.com>
2014-03-19 10:10:42 +0100

    #122 #123 #42

--- example/src/polymer_ui_icon/app_element.dart
+++ example/src/polymer_ui_icon/app_element.dart
@@ -1,24 +1,27 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 
 library app_element;
 
 import 'dart:html';
 import 'package:polymer/polymer.dart';
+import 'package:polymer_ui_elements/polymer_ui_icon/polymer_ui_icon.dart' show
+    PolymerUiIcon;
 
 @CustomTag('app-element')
 class AppElement extends PolymerElement {
-  
+
   @observable
   List icons;
-  
-  AppElement.created() : super.created();
-  
-  enteredView(){
+
+  AppElement.created(): super.created();
+
+  enteredView() {
     super.enteredView();
-    icons = document.createElement('polymer-ui-icon').icons.keys.toList();
+    icons = (document.createElement('polymer-ui-icon') as
+        PolymerUiIcon).icons.keys.toList();
   }
 }
--- example/src/polymer_ui_nav_arrow/app_element.dart
+++ example/src/polymer_ui_nav_arrow/app_element.dart
@@ -1,15 +1,15 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 library polymer_ui_elements.polymer_ui_nav_arrow.app_element;
 
 //import 'dart:async' show Timer;
 import 'dart:html' show CustomEvent, Element, Event;
 import 'package:polymer/polymer.dart' show CustomTag, PolymerElement;
 import 'package:logging/logging.dart' show Logger;
-//import 'package:polymer_ui_elements/polymer_ui_nav_arrow/polymer_ui_nav_arrow.dart' show PolymerUiNavArrow;
+import 'package:polymer_ui_elements/polymer_ui_nav_arrow/polymer_ui_nav_arrow.dart' show PolymerUiNavArrow;
 //import 'package:polymer_elements/polymer_selector/polymer_selector.dart' show PolymerSelector;
 
 @CustomTag('app-element')
@@ -19,10 +19,10 @@ class AppElement extends PolymerElement {
   }
 
   final _logger = new Logger('app-element');
-  
+
   void selectHandler(Event e, details, Element target) {
     if (details['isSelected']) {
-      $['nav-arrow'].target = details['item'];
+      ($['nav-arrow'] as PolymerUiNavArrow).target = details['item'];
     }
   }
 }
