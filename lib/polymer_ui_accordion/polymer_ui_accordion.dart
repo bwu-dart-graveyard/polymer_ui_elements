0c4be35649c89234d0d24ab71ebacd561b836e8d
zoechi <gzoechi@gmail.com>
2014-03-23 18:42:02 +0100

    fixes #126 fixes #127

--- lib/polymer_ui_accordion/polymer_ui_accordion.dart
+++ lib/polymer_ui_accordion/polymer_ui_accordion.dart
@@ -1,8 +1,8 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 library polymer_ui_elements.polymer_ui_accordion;
 
 //import 'dart:html';
@@ -10,7 +10,7 @@ import 'package:polymer/polymer.dart';
 import 'package:polymer_elements/polymer_selector/polymer_selector.dart';
 
 /**
- * polymer-ui-accordion is a simple accordion style element.  It should 
+ * polymer-ui-accordion is a simple accordion style element.  It should
  * be used in conjunction with polymer-ui-collapsible.  By default
  * only one collapsible stays open at a time.  To have multiple
  * collapsibles open set "multi" property on the accordion.
@@ -36,7 +36,7 @@ import 'package:polymer_elements/polymer_selector/polymer_selector.dart';
 class PolymerUiAccordion extends PolymerSelector {
   PolymerUiAccordion.created() : super.created();
 
-  
+
   void activateHandler(e) {
     //TODO Uncomment when notap is supported
     /*
@@ -46,5 +46,4 @@ class PolymerUiAccordion extends PolymerSelector {
     */
     super.activateHandler(e);
   }
-  
 }
