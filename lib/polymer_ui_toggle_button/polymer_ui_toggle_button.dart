d4d9be03ac2d40610f0cad228f24ce55a0a9cd2a
zoechi <gzoechi@gmail.com>
2014-03-24 11:48:04 +0100

    #131

--- lib/polymer_ui_toggle_button/polymer_ui_toggle_button.dart
+++ lib/polymer_ui_toggle_button/polymer_ui_toggle_button.dart
@@ -1,8 +1,8 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 library polymer_ui_elements.polymer_ui_toggle_button;
 
 import 'dart:math';
@@ -23,12 +23,12 @@ class PolymerUiToggleButton extends PolymerElement {
   PolymerUiToggleButton.created() : super.created();
 
   final _logger = new Logger('PolymerUiToggleButton');
-  
+
   /**
    * Gets or sets the state, true is ON and false is OFF.
    */
   @published bool value = false;
-  
+
   /**
    * If true, don't display caption.
    */
@@ -36,39 +36,31 @@ class PolymerUiToggleButton extends PolymerElement {
 
   /**
    * Caption for on state.
-   *
-   * @attribute onCaption
-   * @type string
-   * @default 'ON'
    */
   @published String onCaption = 'ON';
-  
+
   /**
    * Caption for off state.
-   *
-   * @attribute offCaption
-   * @type string
-   * @default 'OFF'
    */
   @published String offCaption = 'OFF';
-  
+
   @observable String currentCaption = '';
   int _w;
   int _x;
-  
+
   void ready() {
     super.ready();
     updateCaption();
   }
-  
+
   void toggle() {
     this.value = !this.value;
   }
-  
+
   void noCaptionChanged(oldValue) {
     this.updateCaption();
   }
-  
+
   void onCaptionChanged(oldValue) {
     this.updateCaption();
   }
@@ -77,7 +69,7 @@ class PolymerUiToggleButton extends PolymerElement {
     this.updateCaption();
   }
 
-  
+
   void updateCaption() {
     if(noCaption) {
       this.currentCaption = '';
@@ -89,31 +81,31 @@ class PolymerUiToggleButton extends PolymerElement {
       }
     }
   }
-  
+
   void valueChanged(oldValue) {
     updateCaption();
     this.classes.toggle('on', this.value);
     this.$['toggle'].classes.toggle('on', this.value);
   }
-  
+
   void trackStart(e) {
     e.preventTap();
     this._w = this.$['toggle'].offsetWidth - this.clientWidth;
     this.$['toggle'].classes.add('dragging');
   }
-  
+
   void track(e) {
     this._x = max(-this._w, min(0, this.value ? e.dx : e.dx - this._w));
     this.$['toggle'].style.left = '${this._x}px';
   }
-  
+
   void trackEnd(e) {
     this.$['toggle'].style.left = null;
     this.$['toggle'].classes.remove('dragging');
     this.value = (this._x).abs() < this._w / 2;
     //Platform.flush();
   }
-  
+
   void flick(e) {
     this.value = e.xVelocity > 0;
     // Platform.flush();
