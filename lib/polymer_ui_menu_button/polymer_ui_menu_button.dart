2f0ac6d7bc5e5899f8e29a14bcd9cd2bd24f0327
zoechi <gzoechi@gmail.com>
2014-03-26 14:05:52 +0100

    fixes #153 #42

--- lib/polymer_ui_menu_button/polymer_ui_menu_button.dart
+++ lib/polymer_ui_menu_button/polymer_ui_menu_button.dart
@@ -1,20 +1,20 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 library polymer_ui_elements.polymer_ui_menu_button;
 
-import 'dart:async' show StreamSubscription, Timer;
+import 'dart:async' show StreamSubscription, Future;
 import 'dart:html' show DeviceOrientationEvent, Element, window;
 import 'package:polymer/polymer.dart' show CustomTag, observable, PolymerElement, published,
-ChangeNotifier, reflectable; // TODO remove ChangeNotifier, reflectable when bug is solved  
+ChangeNotifier, reflectable; // TODO remove ChangeNotifier, reflectable when bug is solved
 // https://code.google.com/p/dart/issues/detail?id=13849
 // (https://code.google.com/p/dart/issues/detail?id=15095)
 import 'package:logging/logging.dart' show Logger;
 
-//import 'package:polymer_ui_elements/polymer_ui_menu/polymer_ui_menu.dart' show PolymerUiMenu;
-//import 'package:polymer_elements/polymer_anchor_point/polymer_anchor_point.dart' show PolymerAnchorPoint;
+import 'package:polymer_ui_elements/polymer_ui_menu/polymer_ui_menu.dart' show PolymerUiMenu;
+import 'package:polymer_elements/polymer_anchor_point/polymer_anchor_point.dart' show PolymerAnchorPoint;
 
 /**
  * polymer-ui-menu-button is a polymer-ui-icon-button with a drop down menu
@@ -38,28 +38,28 @@ class PolymerUiMenuButton extends PolymerElement {
   }
 
   final _logger = new Logger('polymer-ui-menu-button');
-  
+
   /**
    * The icon to display.
    */
   @published String icon = 'dots';
-  
+
   @published String src = '';
-  
+
   /**
    * The index of the selected menu item.
    */
   @published int selected = 0;
-  
-  @published Element selectedItem; 
-  
+
+  @published Element selectedItem;
+
   @published String selectedClass;
-  
+
   /**
    * Set to true to open the menu.
    */
   @published bool opened = false;
-  
+
   /**
    * Horizontally align the overlay with the button. Accepted values are
    * ["left", "center", "right"].
@@ -75,20 +75,20 @@ class PolymerUiMenuButton extends PolymerElement {
   @published bool parallax = false;
   @published bool responsive = false;
   @published String valueattr;
-  
+
   @observable String mediaQuery = 'max-width: 800px';
   @observable String direction = 'up';
-  
-  
+
+
   bool _isParallaxActionRunning = false;
-  
+
   StreamSubscription _deviceorientationSubscription;
   Map<String, int> _tilt;
-  
+
   void openedChanged(oldValue) {
-    new Timer(Duration.ZERO, () {
+    new Future(() {
 //      this._isParallaxActionRunning = true;
-      this.$['arrowPositionHelper'].apply();
+      (this.$['arrowPositionHelper'] as PolymerAnchorPoint).apply();
       this._tilt = null;
       if (this.parallax) {
         window.onDeviceOrientation.listen((e) => parallaxAction(e));
@@ -100,12 +100,12 @@ class PolymerUiMenuButton extends PolymerElement {
       this._isParallaxActionRunning = false;
     });
   }
-  
+
   void parallaxAction(DeviceOrientationEvent e) {
     var tiltLR = 0;
     if (e.gamma != null)  {
       tiltLR = e.gamma.round();
-    } 
+    }
     var tiltTB = 0;
     if (e.beta != null) {
       tiltTB = e.beta.round();
@@ -122,7 +122,7 @@ class PolymerUiMenuButton extends PolymerElement {
       this.$['arrow'].style.transform = 'translate3d(${transX}px,${transY}px,0)';
     }
   }
-  
+
   void mediaChangeAction(e) {
     if (e.detail.matches) {
       this.classes.add('fullwidth');
@@ -130,22 +130,22 @@ class PolymerUiMenuButton extends PolymerElement {
       this.classes.remove('fullwidth');
     }
   }
-  
+
   void closeAction() {
     this.opened = false;
   }
-  
+
   /**
    * Toggle the opened state of the dropdown.
    */
   void toggle() {
     this.opened = !this.opened;
   }
-  
+
   /**
    * The selected menu item.
    */
   Element get selection {
-    return this.$['menu'].selection;
-  }  
+    return (this.$['menu'] as PolymerUiMenu).selection;
+  }
 }
