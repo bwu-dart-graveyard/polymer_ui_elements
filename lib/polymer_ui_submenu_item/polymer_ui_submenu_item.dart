4fa32eda562da0f8d1c6f4ce027b53d685325dfc
zoechi <gzoechi@gmail.com>
2014-03-26 13:58:01 +0100

    fixes #139 fixes #150

--- lib/polymer_ui_submenu_item/polymer_ui_submenu_item.dart
+++ lib/polymer_ui_submenu_item/polymer_ui_submenu_item.dart
@@ -1,14 +1,14 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 library polymer_ui_elements.polymer_ui_submenu_item;
 
 import 'dart:async' show Stream;
 import 'dart:html' show CustomEvent, Element, EventStreamProvider;
 import 'package:polymer/polymer.dart' show CustomTag, observable, PolymerElement, published,
-ChangeNotifier, reflectable; // TODO remove ChangeNotifier, reflectable when bug is solved  
+ChangeNotifier, reflectable; // TODO remove ChangeNotifier, reflectable when bug is solved
 // https://code.google.com/p/dart/issues/detail?id=13849
 // (https://code.google.com/p/dart/issues/detail?id=15095)
 import 'package:logging/logging.dart' show Logger;
@@ -17,7 +17,7 @@ import 'package:polymer_ui_elements/polymer_ui_menu/polymer_ui_menu.dart' show P
 
 /**
  * polymer-ui-submenu-item is a menu-item that can contains other menu-items.
- * It should be used in conjunction with polymer-ui-menu or 
+ * It should be used in conjunction with polymer-ui-menu or
  * polymer-ui-sibebar-menu.
  *
  * Example:
@@ -41,33 +41,34 @@ class PolymerUiSubmenuItem extends PolymerUiMenuItem {
   }
 
   final _logger = new Logger('PolymerUiSubmenuItem');
-  
+
   @published var selected;
   @published Element selectedItem;
-  
+
   @published bool active = false;
-  
+
   @observable bool collapsed = true;
-  
+
   static const EventStreamProvider<CustomEvent> _polymerSelectEvent =
       const EventStreamProvider<CustomEvent>('polymer-select');
-  
+
   Stream<CustomEvent> get onPolymerSelect =>
     PolymerUiSubmenuItem._polymerSelectEvent.forTarget(this);
-  
+
   List<Element> get items {
     return (this.$['menu'] as PolymerUiMenu).items;
   }
-  
+
   bool hasItems() {
     return this.items.length > 0;
   }
-  
+
   void unselectAllItems() {
-    this.$['menu'].selected = null;
-    this.$['menu'].clearSelection();
+    var menu = (this.$['menu'] as PolymerUiMenu);
+    menu.selected = null;
+    menu.clearSelection();
   }
-  
+
   void activeChanged() {
     if (this.hasItems()) {
       this.collapsed = !this.active;
@@ -77,7 +78,7 @@ class PolymerUiSubmenuItem extends PolymerUiMenuItem {
     }
     this.$['item'].classes.toggle('no-active-bg', this.hasItems());
   }
-  
+
   void activate() {
     if (this.hasItems() && this.active) {
       this.collapsed = !this.collapsed;
@@ -85,7 +86,7 @@ class PolymerUiSubmenuItem extends PolymerUiMenuItem {
       this.fire('polymer-select', detail: {'isSelected': true, 'item': this});
     }
   }
-  
+
   int getItemHeight() {
     var item = this.$['item'];
     if(item != null) {
