8eaa52ba0accdff1b43c25fe7ecdae2a2ab4f78d
zoechi <gzoechi@gmail.com>
2014-03-28 11:19:53 +0100

    fix script to download content_shell

--- test/run.sh
+++ test/run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+which content_shell
+if [[ $? -ne 0 ]]; then
+  $DART_SDK/../chromium/download_contentshell.sh
+  unzip content_shell-linux-x64-release.zip
+
+  cs_path=$(ls -d drt-*)
+  PATH=$cs_path:$PATH
+fi
 content_shell --dump-render-tree polymer_collapse.html
