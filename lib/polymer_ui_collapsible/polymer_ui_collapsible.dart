ab630e3b181d02900c010bb1f15309386187098d
zoechi <gzoechi@gmail.com>
2014-03-31 12:16:46 +0200

    #24 remove whitespaces

--- lib/polymer_ui_collapsible/polymer_ui_collapsible.dart
+++ lib/polymer_ui_collapsible/polymer_ui_collapsible.dart
@@ -1,8 +1,8 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 library polymer_ui_elements.polymer_ui_collapsible;
 
 import 'package:polymer/polymer.dart';
@@ -36,19 +36,19 @@ class PolymerUiCollapsible extends PolymerElement {
    */
   @published bool active = false;
 
-  /** 
+  /**
    * Toggle the active state of the collapsible.
    */
   void toggle() {
     this.active = !this.active;
   }
-  
+
   void headerTap() {
     if (!this.notap) {
       this.toggle();
     }
   }
-  
+
   void bodyTap(e) {
     e.notap = true;
   }
